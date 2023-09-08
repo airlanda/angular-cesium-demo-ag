1e4aeaec8d1c8900365cac5962d4a12d9a42dc10
airlanda <45022673+airlanda@users.noreply.github.com>
2023-09-08 17:17:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cesium-demo-ag
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cesium-demo-frsjv7)
---
+++ src/app/map.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'map',
+  template: `
+  <div style="height: 100vh">
+    <ac-map>
+    </ac-map>
+  </div>`,
+})
+export class MapComponent  {
+  @Input() name: string;
+}
